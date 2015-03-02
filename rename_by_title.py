442dfe031843f37c161b39207686d85e00559396
Ceridwen <ceridwenv@gmail.com>
2015-03-01 23:15:26 -0500

    Fix file handling so the temporary file is properly deleted even if the script errors out

--- rename_by_title.py
+++ rename_by_title.py
@@ -11,13 +11,10 @@ from pdfminer.converter import TextConverter
 from pdfminer.cmapdb import CMapDB
 from pdfminer.layout import LAParams
 
-def parse_first_page(pdf_file):
+def parse_first_page(pdf_file, tmp_file):
   rsrcmgr = PDFResourceManager(caching=True)
-  outfp = open('temp_title','w')
-  device = TextConverter(rsrcmgr, outfp, codec='utf-8', laparams=LAParams())
-  pdf = open(pdf_file,'r')
-  process_pdf(rsrcmgr,device,pdf,pagenos=set(),maxpages=2,password='',caching=True,check_extractable=True)
-  outfp.close()
+  device = TextConverter(rsrcmgr, tmp_file, codec='utf-8', laparams=LAParams())
+  process_pdf(rsrcmgr,device,pdf_file,pagenos=set(),maxpages=2,password='',caching=True,check_extractable=True)
   print 'Successfully parsed',pdf_file
 
 def clean_up_and_lower(title_string):
@@ -28,7 +25,7 @@ def clean_up_and_lower(title_string):
   return title_string.lower()
 
 
-def guess_title():
+def guess_title(pdf_text):
   """Tries to guess the title given popular file formats"""
   singles = list(string.ascii_lowercase)
   for x in ['a','i','m','n','e']:
@@ -43,39 +40,38 @@ def guess_title():
 
   lower_bad_title = [x.lower() for x in bad_title_first_words]
 
-  with codecs.open('temp_title','r','utf-8') as pdf_text:
-    for line in pdf_text:
-      title = clean_up_and_lower(line.strip())
+  for line in pdf_text:
+    title = clean_up_and_lower(line.strip())
+    print title
+    print title.split()
+    if title.isdigit() or title == '' or len(title.split()) == 1 or any([x in title for x in lower_bad_title]) or any([y in title.split() for y in singles]) or copyright_notice.match(title) or journal_citation.match(title):
+      continue
+    break
+  else:
+    raise ValueError('No title found.')
+
+  print 'Good title',title
+  print title.split()
+  t = title.split()
+  print len(t)
+  while len(t) == 1:
+    print title
+    ri = raw_input('Would you like to skip this? y/n')
+    if ri == 'y':
+      title = pdf_text.readline().strip()
+      t = title.split()
       print title
-      print title.split()
-      if title.isdigit() or title == '' or len(title.split()) == 1 or any([x in title for x in lower_bad_title]) or any([y in title.split() for y in singles]) or copyright_notice.match(title) or journal_citation.match(title):
-        continue
+
+  # We think we have the beginning of a title
+  rest = ''
+  # Let's join titles that are obviously split over two lines.. 
+  while (not t[-1][0].isupper() and not t[-1][0].isdigit()) or (t[-1][-1] == ':'): 
+    next = pdf_text.readline().strip()
+    if next.isdigit() or next == '' or len(next) == 1 or any([x in next for x in bad_title_first_words]):
       break
     else:
-      raise ValueError('No title found.')
-
-    print 'Good title',title
-    print title.split()
-    t = title.split()
-    print len(t)
-    while len(t) == 1:
-      print title
-      ri = raw_input('Would you like to skip this? y/n')
-      if ri == 'y':
-        title = pdf_text.readline().strip()
-        t = title.split()
-        print title
-
-    # We think we have the beginning of a title
-    rest = ''
-    # Let's join titles that are obviously split over two lines.. 
-    while (not t[-1][0].isupper() and not t[-1][0].isdigit()) or (t[-1][-1] == ':'): 
-      next = pdf_text.readline().strip()
-      if next.isdigit() or next == '' or len(next) == 1 or any([x in next for x in bad_title_first_words]):
-        break
-      else:
-        rest += (' '+next) 
-        t = next.split()
+      rest += (' '+next) 
+      t = next.split()
 
   title = '_'.join((title+rest.lower()).split())
   # Reencode the title into ASCII after normalizing the Unicode.
@@ -89,16 +85,10 @@ def sanitize(title):
   print title
   return title
 
-def get_title(fn):
-  parse_first_page(fn)
-  title= guess_title()
-  return title
-
 def title_rename(title,fn):
   title=sanitize(title)
   os.rename(fn,title+'.pdf')
   print 'Rename of',fn,'complete'
-  os.remove('temp_title')
   print 'Title:',title
   return title+'.pdf'
 
