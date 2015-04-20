76581502ba6342a6e1665b792ae3f8c4a9fe6e0f
Ceridwen <ceridwenv@gmail.com>
2015-04-20 01:45:19 -0400

    Add support for pdftotext and PS files with pstotext, update PDFMiner to new API

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
 pdf_namer
 =========
 
-Renames pdfs from various sources based on their titles, and moves them to a given location.  I run this through cron to clean up my papers directory after I've spent the day downloading uninformatively named pdfs from USENIX or ACM.
+Renames PDFs and PS files from various sources based on their titles,
+and moves them to a given location.  I run this through cron to clean
+up my papers directory after I've spent the day downloading
+uninformatively named PDFs from USENIX or ACM.
 
-Install:
-You need to have Python installed. I have tested this on 2.6 +, but not Python3.
-You also need Yusuke Shinyama's excellent pdfminer library, which you can
-download here: http://www.unixuser.org/~euske/python/pdfminer/
+Install
+-------
 
-To use, make certain that pmv is executable and place it somewhere in your
-executable path.  You will need to either hard code or specify the location of
-rename_by_title.py to the script.  That's pretty much it!
+You need to have Python installed.  Compatible with 2.6+ but not
+Python 3.  For parsing PDFs, you need either pdftotext, which is a
+command-line script distributed as part of Poppler at
+http://poppler.freedesktop.org/ or packaged as poppler-utils in most
+Linux distros, or Yusuke Shinyama's excellent pdfminer library, which
+you can download at https://euske.github.io/pdfminer/ or from PyPi.
+(The Debian version is now too old.)  For parsing PS files, you need
+pstotext, a command line script from
+http://pages.cs.wisc.edu/~ghost/doc/pstotext.htm or distributed by
+most distros.
 
-Example:
+To use, make certain that pmv is executable and place it somewhere in
+your executable path.  You will need to either hard code or specify
+the location of rename_by_title.py to the script.  That's pretty much
+it!
+
+Example
+------
 
 avani@gravitas:~/Downloads $ pmv --dir ~/Desktop/To_Read journal.pone.0094346.pdf
 
--- rename_by_title.py
+++ rename_by_title.py
@@ -1,20 +1,29 @@
+from __future__ import print_function
+
 import codecs
 import os
 import re
 import string
 import sys
 import unicodedata
-from pdfminer.pdfparser import PDFDocument, PDFParser
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter, process_pdf
-from pdfminer.pdfdevice import PDFDevice
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
 from pdfminer.converter import TextConverter
-from pdfminer.cmapdb import CMapDB
 from pdfminer.layout import LAParams
 
-def parse_first_page(pdf_file, tmp_file):
-  rsrcmgr = PDFResourceManager(caching=True)
+
+class TitleError(ValueError):
+  '''Raised when a valid title isn't found.'''
+
+
+def pdf_miner(pdf_file, tmp_file):
+  rsrcmgr = PDFResourceManager()
   device = TextConverter(rsrcmgr, tmp_file, codec='utf-8', laparams=LAParams())
-  process_pdf(rsrcmgr,device,pdf_file,pagenos=set(),maxpages=2,password='',caching=True,check_extractable=True)
+  interpreter = PDFPageInterpreter(rsrcmgr, device)
+  for page in PDFPage.get_pages(pdf_file, maxpages=2):
+      interpreter.process_page(page)
+
 
 def clean_up_and_lower(title_string):
   '''Remove fancy characters that break things'''
@@ -23,8 +32,7 @@ def clean_up_and_lower(title_string):
     title_string = title_string.replace(i,'')
   return title_string.lower()
 
-
-def guess_title(pdf_text):
+def guess_title(txt_name, codec):
   """Tries to guess the title given popular file formats"""
   singles = list(string.ascii_lowercase)
   for x in ['a','i','m','n','e']:
@@ -37,12 +45,13 @@ def guess_title(pdf_text):
     'Conference', 'Symantec Research', 'Symposium', 'Vol', 'IEEE', 'Editor',
     'Published', 'Permissions', 'email', 'doi', 'Higher-Order Symb Comput',
     'University', 'no.', 'Issue', 'pp.', 'Society', 'Report', 'Dissertation',
-    'Thesis', 'Editor', 'Association', 'Computer Science', 'consideration',
+    'Thesis', 'Association', 'Computer Science', 'consideration',
     'publication', 'faculty', 'Department', 'Software Engineering',
     'submission', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
     'Saturday', 'Sunday', 'January', 'February', 'March', 'April', 'June',
     'July', 'August', 'September', 'October', 'November', 'December', 'Journal',
-    'copyright', 'title'
+    'copyright', 'title', 'uptec', 'Oktober', 'examensarbete', 'SIAM', 
+    'computing', 'workshop'
   ]
 
   copyright_notice = re.compile(ur'\(?c\)?\S*\s+\d{4}\s+(\w+\s*)+')
@@ -52,38 +61,46 @@ def guess_title(pdf_text):
 
   lower_bad_title = [x.lower() for x in bad_title_first_words]
 
-  for line in pdf_text:
-    title = clean_up_and_lower(line.strip())
-    print title
-    print title.split()
-    if title.isdigit() or title == '' or len(title.split()) == 1 or any(x in title for x in lower_bad_title) or any(y in title.split() for y in singles) or copyright_notice.match(title) or journal_citation.match(title):
-      continue
-    break
-  else:
-    raise ValueError('No title found.')
-
-  print 'Good title',title
-  print title.split()
-  t = title.split()
-  print len(t)
-  while len(t) == 1:
-    print title
-    ri = raw_input('Would you like to skip this? y/n')
-    if ri == 'y':
-      title = pdf_text.readline().strip()
-      t = title.split()
-      print title
-
-  # We think we have the beginning of a title
-  rest = ''
-  # Let's join titles that are obviously split over two lines.. 
-  while (not t[-1][0].isupper() and not t[-1][0].isdigit()) or (t[-1][-1] == ':'): 
-    next = pdf_text.readline().strip()
-    if next.isdigit() or next == '' or len(next) == 1 or any(x in next for x in bad_title_first_words):
+  with codecs.open(txt_name, 'r', codec) as pdf_text:
+    for line in pdf_text:
+      title = clean_up_and_lower(line.strip())
+      # print(type(title))
+      print(title)
+      print(title.split())
+      if (title.isdigit() or title == '' or len(title.split()) == 1 or
+          any(x in title for x in lower_bad_title) or
+          all(not c.isalpha() for c in title) or
+          # any(y in title.split() for y in singles) or 
+          copyright_notice.match(title) or journal_citation.match(title)):
+        continue
       break
     else:
-      rest += (' '+next) 
-      t = next.split()
+      raise TitleError('No title found.')
+
+    print('Good title %s' % title)
+    print(title.split())
+    t = title.split()
+    print(len(t))
+    while len(t) == 1:
+      print(title)
+      ri = raw_input('Would you like to skip this? y/n')
+      if ri == 'y':
+        title = pdf_text.readline().strip()
+        t = title.split()
+        print(title)
+
+    # We think we have the beginning of a title
+    rest = ''
+    # Let's join titles that are obviously split over two lines.. 
+    while ((not t[-1][0].isupper() and not t[-1][0].isdigit()) or 
+           (t[-1][-1] == ':')):
+      next = pdf_text.readline().strip()
+      if (next.isdigit() or next == '' or len(next) == 1 or 
+          any(x in next for x in bad_title_first_words)):
+        break
+      else:
+        rest += (' '+next) 
+        t = next.split()
 
   title = '_'.join((title+rest.lower()).split())
   # Reencode the title into ASCII after normalizing the Unicode.
@@ -94,15 +111,15 @@ def sanitize(title):
   for i in title:
     if not i.isalnum() and i != '_' and i != '-':
       title = title.replace(i,'')
-  print title
+  print(title)
   return title
 
-def title_rename(title,fn):
+def title_rename(title, fn, extension):
   title=sanitize(title)
-  os.rename(fn,title+'.pdf')
-  print 'Rename of',fn,'complete'
-  print 'Title:',title
-  return title+'.pdf'
+  os.rename(fn, title + extension)
+  print('Rename of %s complete' % fn)
+  print('Title: %s%s' % (title, extension))
+  return title + extension
 
 if __name__ == '__main__':
   fn = sys.argv[1]
